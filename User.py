86163b3ae49109a6e3cdc6ffe6d3b35157b78f75
Jota Landes <jhonatamalta@hotmail.com>
2023-02-05 12:35:03 -0300

    first commit from flask rest app

---
+++ User.py
@@ -0,0 +1,13 @@
+
+
+class User():
+
+    def __init__(self,id,username,password):
+        self.id = id
+        self.username = username
+        self.password = password
+
+    def __str__(self) -> str:
+        return f"User id: {self.id}"
+
+    
